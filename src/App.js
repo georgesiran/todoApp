7b5a3efd82bf2c616f5c89db7ebe12cabad1d165
georgesiran <georgesiran@gmail.com>
2021-01-27 16:57:41 +0200

    project created

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function iterateArray (arr){
+  return arr.map((item)=>{
+    return <li>{item.text}</li>
+  })
+}
+
+function iterateArrayCheckedOnly (arr){
+  return arr.filter((item)=>item.checked).map((item)=>{
+    return <li>{item.text}</li>
+  })
+}
+
+function App() {
+  const arr  = [
+    {text: "water", checked: false},
+    {text: "bread", checked: true},
+    {text: "milk", checked: false},
+    {text: "eggs", checked: true},
+  ];
+  
+  return (
+    <div className="App">
+       {false? iterateArray(arr):iterateArrayCheckedOnly(arr)}
+    </div>
+  );
+}
+
+export default App;
