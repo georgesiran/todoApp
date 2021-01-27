2a935d4fc70ad9b5d6f8c7f84305390b1f1cd110
georgesiran <georgesiran@gmail.com>
2021-01-27 17:45:53 +0200

    lint

--- src/App.js
+++ src/App.js
@@ -1,26 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-import TodoList from './components/TodoList'
-function iterateArray (arr){
-  return arr.map((item)=>{
-    return <li>{item.text}</li>
-  })
-}
-
-function iterateArrayCheckedOnly (arr){
-  return arr.filter((item)=>item.checked).map((item)=>{
-    return <li>{item.text}</li>
-  })
-}
+import TodoList from './components/TodoList';
 
 function App() {
-  const arr  = [
-    {text: "water", checked: false},
-    {text: "bread", checked: true},
-    {text: "milk", checked: false},
-    {text: "eggs", checked: true},
-  ];
-  
   return (
     <div className="App">
       <TodoList></TodoList>
