e5ff9ea989680e285ba88d673d25e6008f18f043
georgesiran <georgesiran@gmail.com>
2021-01-27 17:07:46 +0200

    added component

---
+++ src/components/TodoList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+class TodoList extends React.Component {
+    render (){
+        return (
+            <div> Saluuut</div>
+        )
+    }
+}
+export default TodoList;
